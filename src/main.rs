2d6310aa0254926a40f21eaf21188e6398dc236e
eskevv <hkeven89@gmail.com>
2023-02-01 22:39:09 -0500

    clean main

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,6 @@ use calculator::Calculator;
 fn main() {
   println!("\n ** Rusty-Cal 23 ** \n");
 
-  let h = vec![2, 2, 2, 5];
-
-  for i in h.iter().rev() {
-    println!("{i} : {}", i);
-  }
-
   let problem = "(2 ^ 2 ^ 3 + 7 ^ 2) ^ 8 + (- 87 + 56 / (40 * 2))";
 
   let mut calculator = Calculator::new();
